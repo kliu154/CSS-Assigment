8ea0ce19ae21527a6098f965a6e4d0feb73994f9
kliu154 <123418101+kliu154@users.noreply.github.com>
2023-02-17 19:42:11 -0800

    Delete header directory

--- header/script.js
+++
@@ -1,7 +0,0 @@
-function myFunction1() {
-    document.getElementById("myDropdown1").classList.toggle("show");
-}
-  
-function myFunction2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
-}
